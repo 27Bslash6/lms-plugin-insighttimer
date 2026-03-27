42d5704691712b3af537e9aea8c943794dfdb8b6
Ray Walker <ray@27b.io>
2026-03-27 22:10:29 +1100

    feat: Settings.pm + settings.html — language, page size, HLS preferences

---
+++ Settings.pm
@@ -0,0 +1,16 @@
+package Plugins::InsightTimer::Settings;
+
+use strict;
+use base qw(Slim::Web::Settings);
+
+use Slim::Utils::Prefs;
+
+my $prefs = preferences('plugin.insighttimer');
+
+sub name { Slim::Web::HTTP::CSRF->protectName('PLUGIN_INSIGHTTIMER_NAME') }
+
+sub page { Slim::Web::HTTP::CSRF->protectURI('plugins/InsightTimer/settings.html') }
+
+sub prefs { return ($prefs, qw(language itemsPerPage preferHLS)) }
+
+1;
