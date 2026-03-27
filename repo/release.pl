82cb708a4761364a94b9a1ebcf2f73292194e3b9
Ray Walker <ray@27b.io>
2026-03-27 22:16:28 +1100

    feat: GitHub Actions release workflow + release.pl

---
+++ repo/release.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+
+use XML::Simple;
+use File::Basename;
+use Digest::SHA1;
+
+my $repofile = $ARGV[0];
+my $version = $ARGV[1];
+my $zipfile = $ARGV[2];
+my $url = $ARGV[3];
+
+my $repo = XMLin($repofile, ForceArray => 1, KeepRoot => 0, KeyAttr => 0, NoAttr => 0);
+$repo->{plugins}[0]->{plugin}[0]->{version} = $version;
+
+open (my $fh, "<", $zipfile) or die $!;
+binmode $fh;
+
+my $sha1 = Digest::SHA1->new;
+$sha1->addfile($fh);
+close $fh;
+
+my $hexdigest = $sha1->hexdigest;
+$repo->{plugins}[0]->{plugin}[0]->{sha}[0] = $hexdigest;
+print("version:$version sha:$hexdigest\n");
+
+$url .= "/$zipfile";
+$repo->{plugins}[0]->{plugin}[0]->{url}[0] = $url;
+
+XMLout($repo, RootName => 'extensions', NoSort => 1, XMLDecl => 1, KeyAttr => '', OutputFile => $repofile, NoAttr => 0);
